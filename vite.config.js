7c386bbf0aba96b86f80587ec9711e58035ffbe1
Antonio Lima <antonio@finplace.com.br>
2022-11-06 20:32:08 -0300

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://antonioc57.github.io/chart-prototipo/",
+  base: "/vite-app-sample/",
   plugins: [react()]
 })
